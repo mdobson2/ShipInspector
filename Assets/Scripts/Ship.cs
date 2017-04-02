c0392ba070177d48837efa31b322b1b54f4ee604
Royce R <rroger22@jccc.edu>
2017-04-02 18:39:17 -0500

    Things!
    All the things!

---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    public enum GunStyle { cannon, plasma, machinegun, flamethrower};
+
+    public int armor, attack, agility;
+    public int hitPoints;
+    public GunStyle[] shipGuns;
+    public CrewMember[] crewMembers;
+
+}
